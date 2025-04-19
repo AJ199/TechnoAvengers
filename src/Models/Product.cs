0243bbe072883625be0b51b7999b02a3ee91a72d
Anurag Jindal <anurag.jindal.1996@gmail.com>
2025-04-19 13:23:13 -0700

    Updated the comic hero images and description in the modal window in ProductList.razor, Product.cs and products.json

--- src/Models/Product.cs
+++ src/Models/Product.cs
@@ -1,4 +1,5 @@
-﻿using System.Text.Json;
+﻿using System.Collections.Generic;
+using System.Text.Json;
 using System.Text.Json.Serialization;
 
 namespace ContosoCrafts.WebSite.Models
@@ -6,13 +7,18 @@ namespace ContosoCrafts.WebSite.Models
     public class Product
     {
         public string? Id { get; set; }
-        public string? Maker { get; set; }
-
-        [JsonPropertyName("img")]
-        public string? Image { get; set; }
-        public string? Url { get; set; }
         public string? Title { get; set; }
-        public string? Description { get; set; }
+        public string? Fullname { get; set; }
+        public string? Birthplace { get; set; }
+        public string? Work { get; set; }
+        public string? FirstAppear { get; set; }
+
+
+        [JsonPropertyName("ImageUrl")]
+        public string? ImageUrl { get; set; }
+
+        [JsonPropertyName("Powerstats")]
+        public Dictionary<string, string>? Powerstats { get; set; }
         public int[]? Ratings { get; set; }
 
         public override string ToString() => JsonSerializer.Serialize<Product>(this);
