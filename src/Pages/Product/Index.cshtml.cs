6001ee92b91315ee2b82ae3d51f696abaf96ec73
Anurag Jindal <anurag.jindal.1996@gmail.com>
2025-04-24 11:25:49 -0700

    Updated the cs file to  displays the name of the superhero in Index.cshtml.cs

--- src/Pages/Product/Index.cshtml.cs
+++ src/Pages/Product/Index.cshtml.cs
@@ -1,12 +1,23 @@
-using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Newtonsoft.Json;
+using System.IO;
+using System.Collections.Generic;
+using System.Text.Json.Serialization;
 
 namespace ContosoCrafts.WebSite.Pages.Product
 {
     public class IndexModel : PageModel
     {
+        public List<ContosoCrafts.WebSite.Models.Product> Products { get; set; }
+
         public void OnGet()
         {
+            var filePath = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot", "data", "products.json");
+
+            var json = System.IO.File.ReadAllText(filePath);
+
+            Products = JsonConvert.DeserializeObject<List<ContosoCrafts.WebSite.Models.Product>>(json);
         }
     }
 }
