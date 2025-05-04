e9cb6d711aea6f4603e5412338a727c541f5a445
Anurag Jindal <anurag.jindal.1996@gmail.com>
2025-05-03 23:06:24 -0700

    Create razor page, with prompts for hero information, called Create.cshtml and Create.cshtml.cs

---
+++ src/Pages/Product/Create.cshtml.cs
@@ -0,0 +1,39 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using ContosoCrafts.WebSite.Models;
+using Newtonsoft.Json;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace ContosoCrafts.WebSite.Pages.Product
+{
+    public class CreateModel : PageModel
+    {
+        [BindProperty]
+        public ProductModel Product { get; set; }
+
+        public IActionResult OnGet()
+        {
+            return Page();
+        }
+
+        public IActionResult OnPost()
+        {
+            var filePath = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot", "data", "products.json");
+
+            var json = System.IO.File.ReadAllText(filePath);
+            var products = JsonConvert.DeserializeObject<List<ProductModel>>(json) ?? new List<ProductModel>();
+
+            // Generate a new unique ID
+            Product.Id = (products.Count + 1).ToString();
+
+            products.Add(Product);
+
+            System.IO.File.WriteAllText(filePath, JsonConvert.SerializeObject(products, Formatting.Indented));
+
+            // Redirect to the Read page with the new product's ID
+            return RedirectToPage("Read", new { id = Product.Id });
+        }
+    }
+}
