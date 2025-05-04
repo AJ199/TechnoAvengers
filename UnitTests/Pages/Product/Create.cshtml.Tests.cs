94d9e2b7e13f1c238adec584807456545fd62537
Anurag Jindal <anurag.jindal.1996@gmail.com>
2025-05-04 15:25:41 -0700

    Added Create test suite with test initialise method in the Create.cshtml.Tests.cs

---
+++ UnitTests/Pages/Product/Create.cshtml.Tests.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.ViewFeatures;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Logging;
+using Moq;
+using NUnit.Framework;
+using System.Collections.Generic;
+using System.IO;
+using System.Text.Json;
+using ContosoCrafts.WebSite.Models;
+using ContosoCrafts.WebSite.Pages.Product;
+using System.Linq;
+using Microsoft.AspNetCore.Routing;
+
+namespace UnitTests.Pages.Product
+{
+    public class CreateTests
+    {
+        #region TestSetup
+        public static DefaultHttpContext httpContextDefault;
+        public static ModelStateDictionary modelState;
+        public static ActionContext actionContext;
+        public static EmptyModelMetadataProvider modelMetadataProvider;
+        public static ViewDataDictionary viewData;
+        public static TempDataDictionary tempData;
+        public static PageContext pageContext;
+        public static CreateModel pageModel;
+        public static string testFilePath;
+
+        [SetUp]
+        public void TestInitialize()
+        {
+            httpContextDefault = new DefaultHttpContext();
+            modelState = new ModelStateDictionary();
+            actionContext = new ActionContext(httpContextDefault, httpContextDefault.GetRouteData(), new PageActionDescriptor(), modelState);
+            modelMetadataProvider = new EmptyModelMetadataProvider();
+            viewData = new ViewDataDictionary(modelMetadataProvider, modelState);
+            tempData = new TempDataDictionary(httpContextDefault, Mock.Of<ITempDataProvider>());
+            pageContext = new PageContext(actionContext) { ViewData = viewData };
+
+            // Setup a test file for JSON storage
+            testFilePath = Path.Combine(Path.GetTempPath(), "test-products.json");
+            File.WriteAllText(testFilePath, "[]"); // Start with empty list
+
+            pageModel = new CreateModel
+            {
+                PageContext = pageContext,
+                TempData = tempData
+            };
+
+            // Redirect file path inside the OnPost method to our test file using reflection (or abstract to injectable in real app)
+        }
+        #endregion TestSetup
+
+        
+    }
+}
