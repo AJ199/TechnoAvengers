400e0f1bbc42e551c74c1aed3fc0a1a6b34fd0db
Anurag Jindal <anurag.jindal.1996@gmail.com>
2025-05-04 01:11:33 -0700

    Added test case to check the delete operation id in the Delete.cshtml.Tests.cs

---
+++ UnitTests/Pages/Product/Delete.cshtml.Tests.cs
@@ -0,0 +1,96 @@
+﻿using NUnit.Framework;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
+using Microsoft.AspNetCore.Mvc.ViewFeatures;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Logging;
+using Moq;
+
+using ContosoCrafts.WebSite.Pages.Product;
+using ContosoCrafts.WebSite.Services;
+using ContosoCrafts.WebSite.Models;
+
+using System.Linq;
+using Microsoft.AspNetCore.Routing;
+using Microsoft.AspNetCore.Mvc.Routing;
+
+namespace UnitTests.Pages.Product
+{
+    public class DeleteTests
+    {
+        #region TestSetup
+        public static IUrlHelperFactory urlHelperFactory;
+        public static DefaultHttpContext httpContextDefault;
+        public static IWebHostEnvironment webHostEnvironment;
+        public static ModelStateDictionary modelState;
+        public static ActionContext actionContext;
+        public static EmptyModelMetadataProvider modelMetadataProvider;
+        public static ViewDataDictionary viewData;
+        public static TempDataDictionary tempData;
+        public static PageContext pageContext;
+
+        public static DeleteModel pageModel;
+
+        [SetUp]
+        public void TestInitialize()
+        {
+            httpContextDefault = new DefaultHttpContext()
+            {
+                //RequestServices = serviceProviderMock.Object,
+            };
+
+            modelState = new ModelStateDictionary();
+
+            actionContext = new ActionContext(httpContextDefault, httpContextDefault.GetRouteData(), new PageActionDescriptor(), modelState);
+
+            modelMetadataProvider = new EmptyModelMetadataProvider();
+            viewData = new ViewDataDictionary(modelMetadataProvider, modelState);
+            tempData = new TempDataDictionary(httpContextDefault, Mock.Of<ITempDataProvider>());
+
+            pageContext = new PageContext(actionContext)
+            {
+                ViewData = viewData,
+            };
+
+            var mockWebHostEnvironment = new Mock<IWebHostEnvironment>();
+            mockWebHostEnvironment.Setup(m => m.EnvironmentName).Returns("Hosting:UnitTestEnvironment");
+            mockWebHostEnvironment.Setup(m => m.WebRootPath).Returns("../../../../src/bin/Debug/net8.0/wwwroot");
+            mockWebHostEnvironment.Setup(m => m.ContentRootPath).Returns("./data/");
+
+            var MockLoggerDirect = Mock.Of<ILogger<IndexModel>>();
+            JsonFileProductService productService;
+
+            productService = new JsonFileProductService(mockWebHostEnvironment.Object);
+
+            pageModel = new DeleteModel(productService)
+            {
+            };
+        }
+
+        #endregion TestSetup
+
+        #region OnGet Tests
+
+        [Test]
+        public void OnPost_ValidId_Should_Delete_And_Redirect()
+        {
+            // Arrange
+            var product = TestHelper.ProductService.GetProducts().First();
+            pageModel.Id = product.Id;
+
+            // Act
+            var result = pageModel.OnPost();
+
+            // Assert
+            Assert.IsInstanceOf<RedirectToPageResult>(result);
+            var redirect = result as RedirectToPageResult;
+            Assert.AreEqual("/Product/Index", redirect.PageName);
+        }
+
+
+
+        #endregion OnPost Tests
+    }
+}
