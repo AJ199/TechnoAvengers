5bc4e04531b17371195bab196f5cd17ba17b7788
Anurag Jindal <anurag.jindal.1996@gmail.com>
2025-05-03 17:19:48 -0700

    Modified delete.cshtml.cs file to perform the delete button and save the data

--- src/Pages/Product/Delete.cshtml.cs
+++ src/Pages/Product/Delete.cshtml.cs
@@ -1,12 +1,48 @@
+using System.Linq;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using ContosoCrafts.WebSite.Models;
+using ContosoCrafts.WebSite.Services;
 
 namespace ContosoCrafts.WebSite.Pages.Product
 {
-    public class Index1Model : PageModel
+    public class DeleteModel : PageModel
     {
-        public void OnGet()
+        private readonly JsonFileProductService _productService;
+
+        public DeleteModel(JsonFileProductService productService)
         {
+            _productService = productService;
+        }
+
+        [BindProperty(SupportsGet = true)]
+        public string? Id { get; set; }
+
+        public ProductModel? Product { get; private set; }
+
+        public IActionResult OnGet()
+        {
+            Product = _productService.GetProducts().FirstOrDefault(p => p.Id == Id);
+            if (Product == null)
+            {
+                return RedirectToPage("/Product/Index");
+            }
+
+            return Page();
+        }
+
+        public IActionResult OnPost()
+        {
+            var products = _productService.GetProducts().ToList();
+            var product = products.FirstOrDefault(p => p.Id == Id);
+
+            if (product != null)
+            {
+                products.Remove(product);
+                _productService.SaveData(products); // Ensure SaveData is made public in JsonFileProductService
+            }
+
+            return RedirectToPage("/Product/Index");
         }
     }
 }
