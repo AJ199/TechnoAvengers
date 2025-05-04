94dccb8bd77c02adb1ba8cb8824fc930c3927020
Anurag Jindal <anurag.jindal.1996@gmail.com>
2025-05-03 22:23:55 -0700

    Added test case to check all the products is available or not in the Index.cshtml.Tests.cs

---
+++ UnitTests/Pages/Product/Index.cshtml.Tests.cs
@@ -0,0 +1,58 @@
+using ContosoCrafts.WebSite.Pages.Product;
+using Microsoft.AspNetCore.Mvc;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UnitTests.Pages.Product
+{
+    /// <summary>
+    /// Unit testing for Index Tests
+    /// </summary>
+    public class IndexTests
+    {
+        // Database MiddleTier
+        #region TestSetup
+        public static IndexModel pageModel;
+        /// <summary>
+        /// Initialize of Test
+        /// </summary>
+        [SetUp]
+        public void TestInitialize()
+        {
+            pageModel = new IndexModel();
+        }
+
+        #endregion TestSetup
+        /// <summary>
+        /// Checking whether product user want is there in result or not.
+        /// </summary>
+        #region OnGet
+        [Test]
+        public void OnGet_Valid_Should_Return_Products()
+        {
+            // Arrange
+            var expectedFilePath = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot", "data", "products.json");
+
+            // Confirm the file exists
+            Assert.IsTrue(File.Exists(expectedFilePath), $"JSON file not found at {expectedFilePath}");
+
+            // Act
+            pageModel.OnGet();
+
+            // Assert
+            Assert.IsNotNull(pageModel.Products, "Products list should not be null");
+            Assert.IsNotEmpty(pageModel.Products, "Products list should not be empty");
+
+            // Optionally check a known product
+            var knownProduct = pageModel.Products.FirstOrDefault(p => p.Id == "31");
+            Assert.IsNotNull(knownProduct, "Known product with ID 31 should exist");
+            Assert.AreEqual("Ant-Man II", knownProduct.Title);
+        }
+        #endregion OnGet
+    }
+}
