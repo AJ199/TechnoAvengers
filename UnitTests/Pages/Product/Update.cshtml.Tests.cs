139885d4be6f0cc239c61da11d72f93389177f4f
Anurag Jindal <anurag.jindal.1996@gmail.com>
2025-05-04 18:01:25 -0700

    Added test case for update product to check valid product in the Update.cshtml.Tests.cs

---
+++ UnitTests/Pages/Product/Update.cshtml.Tests.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+using NUnit.Framework;
+
+using ContosoCrafts.WebSite.Pages.Product;
+using ContosoCrafts.WebSite.Models;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.Linq;
+
+namespace UnitTests.Pages.Product
+{
+    /// <summary>
+    /// Provides unit testing for the Update page
+    /// </summary>
+    public class UpdateTests
+    {
+
+        #region TestSetup
+        // Declare the model of the Update page to be used in unit tests
+        public static UpdateModel pageModel;
+
+        [SetUp]
+        /// <summary>
+        /// Initializes mock Update page model for testing.
+        /// </summary>
+        public void TestInitialize()
+        {
+            pageModel = new UpdateModel(TestHelper.ProductService);
+        }
+        #endregion TestSetup
+
+        #region OnGet
+        [Test]
+        /// <summary>
+        /// Verifies OnGet correctly loads product and 
+        /// sets ModelState to valid when a valid ID is provided
+        /// </summary>
+        public void OnGet_ValidId_Should_Return_Product()
+        {
+            // Arrange
+            var id = "1";
+
+            // Act
+            pageModel.OnGet(id);
+
+            // Assert
+            Assert.AreEqual(true, pageModel.ModelState.IsValid);
+            Assert.AreEqual("Spider-Man", pageModel.Product.Title);
+
+            // Reset
+            pageModel.ModelState.Clear();
+
+        }
+
+
+        #endregion OnGet
+
+    }
+}
