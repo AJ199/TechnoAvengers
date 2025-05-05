68582d898c07d5f10e9af1f70b25d3c076be4a75
Anurag Jindal <anurag.jindal.1996@gmail.com>
2025-05-05 08:29:13 -0700

    Resolved warnings of possible null reference assignment by adding safe null fallbacks in Read.cshtml.cs

--- src/Pages/Product/Read.cshtml.cs
+++ src/Pages/Product/Read.cshtml.cs
@@ -6,18 +6,44 @@ using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace ContosoCrafts.WebSite.Pages.Product
 {
+    /// <summary>
+    /// Page model for reading superhero data
+    /// </summary>
     public class ReadModel : PageModel
     {
-        // Data middletier
+        // Service that accesses superhero data
         public JsonFileProductService ProductService { get; }
+
+        // Stores superhero associated with provided ID
+        public ProductModel? Product { get; set; }
+
+        /// <summary>
+        /// Initializes new instance of ReadModel with the given service
+        /// </summary>
+        /// <param name="productService">Service used to retrieve data</param>
         public ReadModel(JsonFileProductService productService)
         {
             ProductService = productService;
         }
-        public ProductModel Product;
+
+        /// <summary>
+        /// Responds to GET requests and loads the appropiate superhero 
+        /// </summary>
+        /// <param name="id">Superhero ID</param>
         public void OnGet(string id)
+        // x.Id == productId
         {
-            Product = ProductService.GetProducts().FirstOrDefault(m => m.Id.Equals(id));
+            // Get the product using the id
+            var retrievedProduct = ProductService.GetProducts().FirstOrDefault(m => m.Id == id);
+
+            if (retrievedProduct == null)
+            {
+                RedirectToPage("/Error");
+                return;
+            }
+
+            // Assigns the product only if it's not null
+            Product = retrievedProduct;
         }
     }
 }
