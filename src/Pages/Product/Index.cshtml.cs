ebd1e849caf6c4d4008d5b401d2dad33ec60dd72
Anurag Jindal <anurag.jindal.1996@gmail.com>
2025-05-12 16:42:40 -0700

    Added comments in Index.cshtml.cs

--- src/Pages/Product/Index.cshtml.cs
+++ src/Pages/Product/Index.cshtml.cs
@@ -9,14 +9,21 @@ namespace ContosoCrafts.WebSite.Pages.Product
 {
     public class IndexModel : PageModel
     {
+        // List of products to be displayed on the page
         public List<ContosoCrafts.WebSite.Models.ProductModel> Products { get; set; }
 
+        /// <summary>
+        /// Handles GET requests to load product data from JSON
+        /// </summary>
         public void OnGet()
         {
+            // Build the full file path to the JSON file
             var filePath = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot", "data", "products.json");
 
+            // Read the contents of the JSON file
             var json = System.IO.File.ReadAllText(filePath);
 
+            // Deserialize the JSON into a list of ProductModel objects
             Products = JsonConvert.DeserializeObject<List<ContosoCrafts.WebSite.Models.ProductModel>>(json);
         }
     }
