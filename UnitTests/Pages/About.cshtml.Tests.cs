1b620c009b532afe472e6c047d69b9e4eda734f9
Anurag Jindal <anurag.jindal.1996@gmail.com>
2025-05-15 03:44:40 -0700

    Added unit tests for About page OnGet handler in about.cshtml.tests.cs

---
+++ UnitTests/Pages/About.cshtml.Tests.cs
@@ -0,0 +1,44 @@
+﻿using ContosoCrafts.WebSite.Pages;
+using NUnit.Framework;
+
+namespace UnitTests.Pages
+{
+    /// <summary>
+    /// Provides unit testing for the About page
+    /// </summary>
+    public class AboutTests
+    {
+        #region TestSetup
+
+        // Page model for About page
+        public static AboutModel pageModel;
+
+        /// <summary>
+        /// Initializes the test environment
+        /// </summary>
+        [SetUp]
+        public void TestInitialize()
+        {
+            pageModel = new AboutModel();
+        }
+
+        #endregion TestSetup
+
+        #region OnGet Tests
+
+        /// <summary>
+        /// Verifies that OnGet executes without errors
+        /// </summary>
+        [Test]
+        public void OnGet_Should_Work_Without_Error()
+        {
+            // Act
+            pageModel.OnGet();
+
+            // Assert
+            Assert.IsNotNull(pageModel);
+        }
+
+        #endregion OnGet Tests
+    }
+}
