0fb6eadb1ebb7dd9d1e1a57ef3f67a66cdd536a5
Anurag Jindal <anurag.jindal.1996@gmail.com>
2025-05-15 03:52:02 -0700

    Added unit test for NewsModel OnGet method in news.cshtml.tests.cs

---
+++ UnitTests/Pages/News.cshtml.Tests.cs
@@ -0,0 +1,44 @@
+﻿using ContosoCrafts.WebSite.Pages;
+using NUnit.Framework;
+
+namespace UnitTests.Pages
+{
+    /// <summary>
+    /// Provides unit testing for the News page
+    /// </summary>
+    public class NewsTests
+    {
+        #region TestSetup
+
+        // Page model for News page
+        public static NewsModel pageModel;
+
+        /// <summary>
+        /// Initializes the test environment
+        /// </summary>
+        [SetUp]
+        public void TestInitialize()
+        {
+            pageModel = new NewsModel();
+        }
+
+        #endregion TestSetup
+
+        #region OnGet Tests
+
+        /// <summary>
+        /// Verifies that OnGet executes without errors
+        /// </summary>
+        [Test]
+        public void OnGet_Should_Work_Without_Error()
+        {
+            // Act
+            pageModel.OnGet();
+
+            // Assert
+            Assert.IsNotNull(pageModel);
+        }
+
+        #endregion OnGet Tests
+    }
+}
