eb62b283d3f2bec93a145bb06066c14653846708
Anurag Jindal <anurag.jindal.1996@gmail.com>
2025-05-15 03:47:35 -0700

    Added unit test for AvengersHistoryModel OnGet method inavengershistory.cshtml.tests.cs

---
+++ UnitTests/Pages/AvengersHistory.cshtml.Tests.cs
@@ -0,0 +1,44 @@
+﻿using ContosoCrafts.WebSite.Pages;
+using NUnit.Framework;
+
+namespace UnitTests.Pages
+{
+    /// <summary>
+    /// Provides unit testing for the Avengers History page
+    /// </summary>
+    public class AvengersHistoryTests
+    {
+        #region TestSetup
+
+        // Page model for Avengers History page
+        public static AvengersHistoryModel pageModel;
+
+        /// <summary>
+        /// Initializes the test environment
+        /// </summary>
+        [SetUp]
+        public void TestInitialize()
+        {
+            pageModel = new AvengersHistoryModel();
+        }
+
+        #endregion TestSetup
+
+        #region OnGet Tests
+
+        /// <summary>
+        /// Verifies that OnGet executes without errors
+        /// </summary>
+        [Test]
+        public void OnGet_Should_Work_Without_Error()
+        {
+            // Act
+            pageModel.OnGet();
+
+            // Assert
+            Assert.IsNotNull(pageModel);
+        }
+
+        #endregion OnGet Tests
+    }
+}
