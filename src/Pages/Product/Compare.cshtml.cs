2966e921f514d37db0ad9bb07986b29074c74a75
Anurag Jindal <anurag.jindal.1996@gmail.com>
2025-05-16 18:25:37 -0700

    Implemented logic to redirect to CompareResult page with selected heroes in compare.cshtml.cs

--- src/Pages/Product/Compare.cshtml.cs
+++ src/Pages/Product/Compare.cshtml.cs
@@ -1,12 +1,44 @@
+using ContosoCrafts.WebSite.Models;
+using ContosoCrafts.WebSite.Services;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace ContosoCrafts.WebSite.Pages.Product
 {
-    public class Index1Model : PageModel
+    public class CompareModel : PageModel
     {
+        private readonly JsonFileProductService _productService;
+
+        public CompareModel(JsonFileProductService productService)
+        {
+            _productService = productService;
+        }
+
+        public List<ProductModel> Products { get; set; } = new();
+        public ProductModel? Hero1 { get; set; }
+        public ProductModel? Hero2 { get; set; }
+
+        [BindProperty]
+        public string? Hero1Id { get; set; }
+
+        [BindProperty]
+        public string? Hero2Id { get; set; }
+
         public void OnGet()
         {
+            Products = _productService.GetProducts().ToList();
+        }
+
+        public IActionResult OnPost()
+        {
+            if (Hero1Id == Hero2Id || string.IsNullOrEmpty(Hero1Id) || string.IsNullOrEmpty(Hero2Id))
+            {
+                return Page(); // Validation fallback
+            }
+
+            return RedirectToPage("CompareResult", new { hero1Id = Hero1Id, hero2Id = Hero2Id });
         }
     }
 }
