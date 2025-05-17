b34bef3a62ef83664bfa094714ed0bb50d06c1d3
Anurag Jindal <anurag.jindal.1996@gmail.com>
2025-05-16 18:27:40 -0700

    Loaded superhero data for CompareResult page using product service in compareresult.cshtml.cs

--- src/Pages/Product/CompareResult.cshtml.cs
+++ src/Pages/Product/CompareResult.cshtml.cs
@@ -1,12 +1,33 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using ContosoCrafts.WebSite.Models;
+using ContosoCrafts.WebSite.Services;
+using System.Linq;
 
 namespace ContosoCrafts.WebSite.Pages.Product
 {
-    public class Index1Model : PageModel
+    public class CompareResultModel : PageModel
     {
-        public void OnGet()
+        private readonly JsonFileProductService _productService;
+
+        public CompareResultModel(JsonFileProductService productService)
         {
+            _productService = productService;
+        }
+
+        public ProductModel Hero1 { get; set; }
+        public ProductModel Hero2 { get; set; }
+
+        public IActionResult OnGet(string hero1Id, string hero2Id)
+        {
+            var all = _productService.GetProducts().ToList();
+
+            Hero1 = all.FirstOrDefault(x => x.Id == hero1Id);
+            Hero2 = all.FirstOrDefault(x => x.Id == hero2Id);
+
+            if (Hero1 == null || Hero2 == null) return RedirectToPage("Compare");
+
+            return Page();
         }
     }
 }
