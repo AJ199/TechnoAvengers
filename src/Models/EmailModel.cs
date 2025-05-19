5de01a21b5f8c5509ac7eaeb755418e5c5cc81c7
Anurag Jindal <anurag.jindal.1996@gmail.com>
2025-05-19 04:55:11 -0700

    Added properties in EmailModel.cs

--- src/Models/EmailModel.cs
+++ src/Models/EmailModel.cs
@@ -1,1 +1,26 @@
-﻿
+﻿namespace ContosoCrafts.WebSite.Models
+{
+    /// <summary>
+    /// Represents the configuration for sending emails
+    /// </summary>
+    public class EmailModel
+    {
+        // The SMTP server address
+        public string SmtpServer { get; set; }
+
+        // The port number for the SMTP server
+        public int Port { get; set; }
+
+        // The name of the sender
+        public string SenderName { get; set; }
+
+        // The email address of the sender
+        public string SenderEmail { get; set; }
+
+        // The username for authenticating with the SMTP server
+        public string Username { get; set; }
+
+        // The password for authenticating with the SMTP server
+        public string Password { get; set; }
+    }
+}
