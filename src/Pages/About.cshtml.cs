eadaf92342c8e20e4a17d8e8e1b2dee673c31905
Anurag Jindal <anurag.jindal.1996@gmail.com>
2025-05-20 16:50:25 -0700

    Added comments in About.cshtml.cs

--- src/Pages/About.cshtml.cs
+++ src/Pages/About.cshtml.cs
@@ -2,10 +2,17 @@ using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace ContosoCrafts.WebSite.Pages
 {
+    /// <summary>
+    /// Handles the logic for displaying the About Razor Page.
+    /// </summary>
     public class AboutModel : PageModel
     {
+        /// <summary>
+        /// Handles GET requests to load the About page.
+        /// </summary>
         public void OnGet()
         {
+            // No data processing is needed
         }
     }
 }
