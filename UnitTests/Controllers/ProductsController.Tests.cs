be321f8ebb662ef71f4f6d12206b4ccadd099bd8
Anurag Jindal <anurag.jindal.1996@gmail.com>
2025-05-19 22:10:17 -0700

    Added the new controller test suite in the ProductsController.Tests.cs

---
+++ UnitTests/Controllers/ProductsController.Tests.cs
@@ -0,0 +1,105 @@
+﻿using NUnit.Framework;
+using System.Linq;
+using ContosoCrafts.WebSite.Controllers;
+using Microsoft.AspNetCore.Mvc;
+
+
+namespace UnitTests.Controllers
+{
+    /// <summary>
+    /// Unit tests for ProductsController class
+    /// </summary>
+    public class ProductsControllerTest
+    {
+        #region TestSetup
+        /// <summary>
+        /// Test Setup
+        /// </summary>
+        [SetUp]
+        public void TestInitialize()
+        {
+        }
+        #endregion
+
+        #region
+        /// <summary>
+        /// Creates a default datapoint of ProductServices
+        /// Creates a new datapoint of the a ProductsController with datapoint
+        /// Gets the whole datapoint
+        /// Tests if Equal
+        /// </summary>
+        [Test]
+        public void get_AllData_Present_Should_Return_True()
+        {
+            //arrange
+            //Create new default ProductService datapoint
+
+            //Act
+            //store datapoint as a ProductController datapoint
+            var newData = new ProductsController(TestHelper.ProductService).Get().First();
+
+            var response = TestHelper.ProductService.GetProducts().First();
+
+            //Assert
+            Assert.AreEqual(newData.Id, response.Id);
+        }
+
+        #endregion
+
+        #region
+        /// <summary>
+        /// Creates a default datapoint of ProductServices
+        /// Creates a new datapoint of the a ProductsController with datapoint
+        /// Gets the whole datapoint
+        /// Tests if Added dataPoint equals the created one
+        /// </summary>
+        [Test]
+        public void Patch_AddValid_Rating_Should_Return_True()
+        {
+            //arrange
+            //Create new default ProductService datapoint
+
+            //Act
+            //store datapoint as a ProductController datapoint
+            var newData = new ProductsController(TestHelper.ProductService);
+            //Create a newRating datapoint to "Patch to theDataController"
+            var newRating = new ProductsController.RatingRequest();
+            {
+                newRating.ProductId = newData.ProductService.GetProducts().Last().Id;
+                newRating.Rating = 5;
+            }
+
+            //Act
+            newData.Patch(newRating);
+
+            //Assert
+            Assert.AreEqual(newData.ProductService.GetProducts().Last().Id, newRating.ProductId);
+        }
+
+        /// <summary>
+        /// Verifies that Patch returns BadRequest (400) when the RatingRequest is null or has a null ProductId.
+        /// </summary>
+        [Test]
+        public void Patch_NullOrMissingProductId_Returns_BadRequest()
+        {
+            // Arrange
+            var controller = new ProductsController(TestHelper.ProductService);
+
+            // Act & Assert
+            // Case 1: request is null
+            var resultNull = controller.Patch(null);
+            Assert.That(resultNull, Is.TypeOf<BadRequestResult>());
+
+            // Case 2: request with null ProductId
+            var requestMissingId = new ProductsController.RatingRequest
+            {
+                ProductId = null,
+                Rating = 3
+            };
+            var resultMissingId = controller.Patch(requestMissingId);
+            Assert.That(resultMissingId, Is.TypeOf<BadRequestResult>());
+        }
+
+        #endregion
+    }
+}
