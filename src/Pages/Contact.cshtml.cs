8b850711219f5a6e3b269d4675fad7a1f87e5545
Anurag Jindal <anurag.jindal.1996@gmail.com>
2025-05-20 17:08:16 -0700

    Added comments in Contact.cshtml.cs

--- src/Pages/Contact.cshtml.cs
+++ src/Pages/Contact.cshtml.cs
@@ -2,10 +2,17 @@ using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace ContosoCrafts.WebSite.Pages
 {
+    /// <summary>
+    /// Handles the logic for displaying the Contact Razor Page.
+    /// </summary>
     public class ContactModel : PageModel
     {
+        /// <summary>
+        /// Handles GET requests to load the Contact page
+        /// </summary>
         public void OnGet()
         {
+            // No data processing is needed
         }
     }
 }
