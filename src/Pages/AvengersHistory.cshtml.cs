b90e88b874f37a275315c407c1e70b530bdaa50e
Anurag Jindal <anurag.jindal.1996@gmail.com>
2025-05-20 19:50:07 -0700

    Added comments in News.cshtml.cs

--- src/Pages/AvengersHistory.cshtml.cs
+++ src/Pages/AvengersHistory.cshtml.cs
@@ -3,16 +3,16 @@ using Microsoft.AspNetCore.Mvc.RazorPages;
 namespace ContosoCrafts.WebSite.Pages
 {
     /// <summary>
-    /// Handles the logic for displaying the AvengersHistory Razor Page.
+    /// Handles the logic for displaying the AvengersHistory Razor Page
     /// </summary>
     public class AvengersHistoryModel : PageModel
     {
         /// <summary>
-        /// Handles GET requests to load the AvengersHistory page.
+        /// Handles GET requests to load the AvengersHistory page
         /// </summary>
         public void OnGet()
         {
-            // No data processing is needed.
+            // No data processing is needed
         }
     }
 }
--- src/Pages/News.cshtml.cs
+++ src/Pages/News.cshtml.cs
@@ -2,10 +2,17 @@ using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace ContosoCrafts.WebSite.Pages
 {
+    /// <summary>
+    /// Handles the logic for displaying the News Razor Page
+    /// </summary>
     public class NewsModel : PageModel
     {
+        /// <summary>
+        /// Handles GET requests to load the News page
+        /// </summary>
         public void OnGet()
         {
+            // No data processing is needed
         }
     }
 }
