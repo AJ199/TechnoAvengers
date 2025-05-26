97cba3ffb522244e89a24b341bc8193b3c815f85
Anurag Jindal <anurag.jindal.1996@gmail.com>
2025-05-26 04:03:55 -0700

    Added test cases for discover page in discover.cshtml.tests.cs

---
+++ UnitTests/Pages/Discover.cshtml.Tests.cs
@@ -0,0 +1,45 @@
+﻿using ContosoCrafts.WebSite.Pages;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using NUnit.Framework;
+
+namespace UnitTests.Pages
+{
+    /// <summary>
+    /// Unit tests for the Discover page.
+    /// </summary>
+    public class DiscoverTests
+    {
+        #region TestSetup
+
+        // The Discover page model for testing
+        public static DiscoverModel pageModel;
+
+        /// <summary>
+        /// Initializes the test environment
+        /// </summary>
+        [SetUp]
+        public void TestInitialize()
+        {
+            pageModel = new DiscoverModel();
+        }
+
+        #endregion TestSetup
+
+        #region OnGet
+
+        /// <summary>
+        /// Ensures the page loads correctly and title is set.
+        /// </summary>
+        [Test]
+        public void OnGet_Valid_Should_Set_Title()
+        {
+            // Arrange & Act
+            pageModel.OnGet();
+
+            // Assert
+            Assert.AreEqual("Discover Your Avenger", pageModel.ViewData["Title"]);
+        }
+
+        #endregion OnGet
+    }
+}
