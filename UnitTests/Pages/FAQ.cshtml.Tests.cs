a8ecab89e583183697d6d0af246c23978a52ef1a
Anurag Jindal <anurag.jindal.1996@gmail.com>
2025-05-26 04:07:32 -0700

    Added test cases for FAQ page in faq.cshtml.tests.cs

---
+++ UnitTests/Pages/FAQ.cshtml.Tests.cs
@@ -0,0 +1,38 @@
+﻿using ContosoCrafts.WebSite.Pages;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using NUnit.Framework;
+
+namespace UnitTests.Pages
+{
+    /// <summary>
+    /// Unit tests for the FAQ page.
+    /// </summary>
+    public class FAQTests
+    {
+        #region TestSetup
+
+        public static FAQModel pageModel;
+
+        [SetUp]
+        public void TestInitialize()
+        {
+            pageModel = new FAQModel();
+        }
+
+        #endregion TestSetup
+
+        #region OnGet
+
+        [Test]
+        public void OnGet_Valid_Should_Set_Title()
+        {
+            // Act
+            pageModel.OnGet();
+
+            // Assert
+            Assert.AreEqual("Frequently Asked Questions", pageModel.ViewData["Title"]);
+        }
+
+        #endregion
+    }
+}
