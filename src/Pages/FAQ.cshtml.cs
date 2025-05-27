cb4da35ca6ee16c03bc070db24a38b8b21b9350a
Anurag Jindal <anurag.jindal.1996@gmail.com>
2025-05-27 09:29:46 -0700

    Added comments in FAQ.cshtml.cs

--- src/Pages/FAQ.cshtml.cs
+++ src/Pages/FAQ.cshtml.cs
@@ -2,10 +2,17 @@ using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace ContosoCrafts.WebSite.Pages
 {
+    /// <summary>
+    /// Handles the logic for displaying the FAQ Razor Page
+    /// </summary>
     public class FAQModel : PageModel
     {
+        /// <summary>
+        /// Handles GET requests to load the AvengersHistory page
+        /// </summary>
         public void OnGet()
         {
+            // No data processing is needed
         }
     }
 }
