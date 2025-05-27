a5ec880b9a240528895786e3ee8fe7b592f9671f
Anurag Jindal <anurag.jindal.1996@gmail.com>
2025-05-26 23:11:35 -0700

    Added properties in EmailSettingsModel.cs

--- src/Models/EmailSettingsModel.cs
+++ src/Models/EmailSettingsModel.cs
@@ -1,6 +1,35 @@
-﻿namespace ContosoCrafts.WebSite.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ContosoCrafts.WebSite.Models
 {
+    /// <summary>
+    /// Represents SMTP settings loaded from configuration
+    /// </summary>
     public class EmailSettingsModel
     {
+        // SMTP server address
+        [Required(ErrorMessage = "SMTP server is required")]
+        public string SmtpServer { get; set; }
+
+        // SMTP port
+        [Required(ErrorMessage = "Port number is required")]
+        public int Port { get; set; }
+
+        // Display name of the sender
+        [Required(ErrorMessage = "Sender name is required")]
+        public string SenderName { get; set; }
+
+        // Sender's email address
+        [Required(ErrorMessage = "Sender email is required")]
+        [EmailAddress(ErrorMessage = "Sender email must be a valid email address")]
+        public string SenderEmail { get; set; }
+
+        // Username for SMTP authentication
+        [Required(ErrorMessage = "Username is required")]
+        public string Username { get; set; }
+
+        // Password for SMTP authentication
+        [Required(ErrorMessage = "Password is required")]
+        public string Password { get; set; }
     }
 }
