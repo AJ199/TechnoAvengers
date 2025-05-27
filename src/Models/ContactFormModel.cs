f0dffb545158a7fce66d65b593ca0c9cb2085edf
Anurag Jindal <anurag.jindal.1996@gmail.com>
2025-05-26 23:14:06 -0700

    Added properties in ContactFormModel.cs

--- src/Models/ContactFormModel.cs
+++ src/Models/ContactFormModel.cs
@@ -1,6 +1,16 @@
-﻿namespace ContosoCrafts.WebSite.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ContosoCrafts.WebSite.Models
 {
+    /// <summary>
+    /// Represents the contact form data submitted by the user
+    /// </summary>
     public class ContactFormModel
     {
+        public string Name { get; set; }
+
+        public string Email { get; set; }
+
+        public string Message { get; set; }
     }
 }
