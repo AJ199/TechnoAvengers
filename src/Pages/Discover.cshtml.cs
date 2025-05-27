efe46c44284c07cbcceebd91de2690494fdfddff
Anurag Jindal <anurag.jindal.1996@gmail.com>
2025-05-27 09:33:53 -0700

    Added comments in Discover.cshtml.cs

--- src/Pages/Discover.cshtml.cs
+++ src/Pages/Discover.cshtml.cs
@@ -2,8 +2,14 @@ using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace ContosoCrafts.WebSite.Pages
 {
+    /// <summary>
+    /// Handles the logic for displaying the Discover Razor Page
+    /// </summary>
     public class DiscoverModel : PageModel
     {
+        /// <summary>
+        /// Handles GET requests to load the Discover page
+        /// </summary>
         public void OnGet()
         {
             ViewData["Title"] = "Discover Your Avenger";
