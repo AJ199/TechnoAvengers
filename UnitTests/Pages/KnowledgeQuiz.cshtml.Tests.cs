104d6b793a56f6c5dfa6f2588b2f7f56f559c38a
Anurag Jindal <anurag.jindal.1996@gmail.com>
2025-05-26 18:16:08 -0700

    Added unit tests for knowledge quiz page in knowledgequiz.cshtml.tests.cs

---
+++ UnitTests/Pages/KnowledgeQuiz.cshtml.Tests.cs
@@ -0,0 +1,50 @@
+﻿using ContosoCrafts.WebSite.Pages;
+using NUnit.Framework;
+using System.Collections.Generic;
+
+namespace UnitTests.Pages
+{
+    public class KnowledgeQuizModelTests
+    {
+        private KnowledgeQuizModel pageModel;
+
+        [SetUp]
+        public void Setup()
+        {
+            pageModel = new KnowledgeQuizModel();
+        }
+
+        [Test]
+        public void OnGet_Should_Populate_CorrectAnswers()
+        {
+            // Act
+            pageModel.OnGet();
+
+            // Assert
+            Assert.AreEqual(10, pageModel.CorrectAnswers.Count);
+            Assert.AreEqual("Captain America", pageModel.CorrectAnswers[0]);
+        }
+
+        [Test]
+        public void Questions_Setter_Should_Assign_New_Questions()
+        {
+            // Arrange
+            var newQuestions = new List<KnowledgeQuizModel.QuestionModel>
+            {
+                new KnowledgeQuizModel.QuestionModel
+                {
+                    Question = "Test?",
+                    Options = new[] { "A", "B", "C" },
+                    Answer = "A"
+                }
+            };
+
+            // Act
+            pageModel.Questions = newQuestions;
+
+            // Assert
+            Assert.AreEqual(1, pageModel.Questions.Count);
+            Assert.AreEqual("Test?", pageModel.Questions[0].Question);
+        }
+    }
+}
