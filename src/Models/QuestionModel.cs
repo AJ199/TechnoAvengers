e1a412a59a22db7521c7c4e3095acca010becbc9
Anurag Jindal <anurag.jindal.1996@gmail.com>
2025-05-27 10:02:31 -0700

    Added properties to QuestionModel.cs

--- src/Models/QuestionModel.cs
+++ src/Models/QuestionModel.cs
@@ -1,6 +1,22 @@
-﻿namespace ContosoCrafts.WebSite.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ContosoCrafts.WebSite.Models
 {
+    /// <summary>
+    /// Respresents a question of the knowledge quiz
+    /// </summary>
     public class QuestionModel
     {
+        // Question 
+        [Required]
+        public string Question { get; set; }
+
+        // Options the user can choose from
+        [Required]
+        public string[] Options { get; set; }
+
+        // Correct answer
+        [Required]
+        public string Answer { get; set; }
     }
 }
