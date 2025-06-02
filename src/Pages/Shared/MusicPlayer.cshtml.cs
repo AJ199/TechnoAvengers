2cd3de421fb7a22dbc10eb0aa2f19380d80602e5
Anurag Jindal <anurag.jindal.1996@gmail.com>
2025-06-01 21:25:18 -0700

    Add comments for MusicPlayer.cshtml.cs

--- src/Pages/Shared/MusicPlayer.cshtml.cs
+++ src/Pages/Shared/MusicPlayer.cshtml.cs
@@ -3,8 +3,15 @@ using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace ContosoCrafts.WebSite.Pages.Shared
 {
+    /// <summary>
+    /// This page can be used for shared UI components or content 
+    /// that are displayed across multiple pages primarily a music player.
+    /// </summary>
     public class IndexModel : PageModel
     {
+        /// <summary>
+        /// Handles GET requests to the page.
+        /// </summary>
         public void OnGet()
         {
         }
