f4a8eeff618f0cae98529d7d7d69aef9d415eab6
Anurag Jindal <anurag.jindal.1996@gmail.com>
2025-06-05 17:04:11 -0700

    Created model to store Yes and No vote counts in pollresultmodel.cs

---
+++ src/Models/PollResultModel.cs
@@ -0,0 +1,18 @@
+﻿namespace ContosoCrafts.WebSite.Models
+{
+    /// <summary>
+    /// Represents the result of a poll with vote counts for "Yes" and "No" responses.
+    /// </summary>
+    public class PollResultModel
+    {
+        /// <summary>
+        /// Total number of "Yes" votes recorded.
+        /// </summary>
+        public int YesVotes { get; set; }
+
+        /// <summary>
+        /// Total number of "No" votes recorded.
+        /// </summary>
+        public int NoVotes { get; set; }
+    }
+}
